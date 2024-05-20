f9f780395163ccd62b9a10ba654aec7449c7af37
Lindsey Adyson Huddleston <lindseyadysonhuddleston@Lindseys-iMac.attlocal.net>
2024-05-20 10:11:05 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//string: You have received this message because you have been chosen to open an important vault. Here is the secret combination:
+alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination:");
+// vault combination variable 10
+const num1=5 + 5;
+// vault combination variable 40
+const num2=10 * 4;
+// vault combination variable 39
+const num3=117 / 3;
+// popup vault code combination variables
+const combination=num1 + " - " + num2 + " - " + num3;
+//popup vault code combination
+const greeting = "Vault" + " " + "Code" + " " + "Combination" + " ";
+alert(greeting + combination)
